58381d7e91b42b74f4fc4fa4e2c6f4127227eb64
Aimable01 <aimable.kwizera14@gmail.com>
2025-10-22 09:56:12 +0200

    feat: register routes

--- term1 Project 0/tms_backend/src/index.ts
+++ term1 Project 0/tms_backend/src/index.ts
@@ -1,8 +1,17 @@
 import { openapi } from "@elysiajs/openapi";
 import { Elysia } from "elysia";
+import mongoose from "mongoose";
 
 export const app = new Elysia().use(openapi()).listen(3000);
 app.get("/", () => "Hello Elysia");
+
+// db connection
+mongoose.connect("mongodb://localhost:27017");
+
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error")
+);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
